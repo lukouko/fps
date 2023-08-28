4c9a5d882e2f4d1ad226fe68d2202d331b7f083c
lukouko <lukouko@gmail.com>
2023-08-28 22:44:33 +1000

    fizzbash

--- game-engine/src/assets/index.js
+++ game-engine/src/assets/index.js
@@ -7,8 +7,6 @@ import tiles2 from './tiles_2_256_256.jpg';
 import plaster1 from './plaster_1_256_256.jpg';
 import asphalt1 from './asphalt_1_256_256.jpg';
 import horizontalTimber1 from './horizontal_timber_1_256_256.jpg';
-import gun1 from './gun1.png';
-import gun2 from './gun2.png';
 import underConstruction from './under_construction.jpg';
 
 // Sprites.
@@ -24,9 +22,6 @@ const images = [
   { id: 'plaster_1', assetPath: plaster1 },
   { id: 'asphalt_1', assetPath: asphalt1 },
   { id: 'horizontal_timber_1', assetPath: horizontalTimber1 },
-  { id: 'gun1', assetPath: gun1, isSprite: true },
-  { id: 'gun2', assetPath: gun2 },
-  { id: 'gun2', assetPath: gun2 },
   { id: 'under_construction', assetPath: underConstruction },
   { id: 'better-looking-matt', assetPath: betterLookingMatt, isSprite: true },
   // { id: 'clouds1', assetPath: clouds1, isRepeatable: true },
